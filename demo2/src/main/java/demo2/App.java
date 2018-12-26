50132b5d0c35965f846bc82631da7af335d9f29c
ravikumarrv <ravikumarrv@gmail.com>
2018-12-26 08:27:28 +0530

    demo

---
+++ demo2/src/main/java/demo2/App.java
@@ -0,0 +1,14 @@
+package demo2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World to Devops!" );
+    }
+}
